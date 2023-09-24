db8e1cd0fd49a4b0292df48ecfafd5dfbab0d923
msu-bit-taksin-63010914604 <119407074+msu-bit-taksin-63010914604@users.noreply.github.com>
2023-09-24 15:42:09 +0700

    Add files via upload

---
+++ customers/show.blade.php
@@ -0,0 +1,35 @@
+@extends('customers.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Customer</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('customers.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $customer->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Phone:</strong>
+                {{ $customer->num }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Address:</strong>
+                {{ $customer->detail }}
+            </div>
+        </div>
+    </div>
+@endsection
